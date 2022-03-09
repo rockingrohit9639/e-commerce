67640d775f358f1ddc93d38d2bd42a7377c77142
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-03-09 10:56:21 +0530

    Backend: Added get all users

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -50,6 +50,21 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     catch(err) {
         return res.status(500).json(err);
     }
+});
+
+// GET ALL USER
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+
+    try{
+        const users = query ? await User.find().sort({ _id: -1}).limit(5) : await User.find();
+        // const {password, ...others} = user._doc;
+        res.status(200).json(users);
+    }
+    catch(err) {
+        return res.status(500).json(err);
+    }
 })
+
 
 module.exports = router;
